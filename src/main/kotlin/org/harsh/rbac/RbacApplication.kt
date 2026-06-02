4b51f06005225053549fe89ebd7b62ce5a52f8c3
1-harshr <rharsh.iitk@gmail.com>
2026-06-02 22:31:00 +0530

    initial commit

---
+++ src/main/kotlin/org/harsh/rbac/RbacApplication.kt
@@ -0,0 +1,11 @@
+package org.harsh.rbac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RbacApplication
+
+fun main(args: Array<String>) {
+    runApplication<RbacApplication>(*args)
+}
---
+++ src/test/kotlin/org/harsh/rbac/RbacApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.harsh.rbac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RbacApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
