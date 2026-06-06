e70318efa06001796472060fc726758b511f5f38
1-harshr <rharsh.iitk@gmail.com>
2026-06-06 23:58:23 +0530

    added UserAlreadyExists Error

--- src/main/kotlin/org/harsh/rbac/service/UserService.kt
+++ src/main/kotlin/org/harsh/rbac/service/UserService.kt
@@ -1,9 +1,11 @@
 package org.harsh.rbac.service
 
 import org.harsh.rbac.dto.UserDto
+import org.harsh.rbac.exception.UserAlreadyExists
 import org.harsh.rbac.mapper.toDto
 import org.harsh.rbac.mapper.toEntity
 import org.harsh.rbac.repo.UserRepo
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
@@ -16,7 +18,11 @@ class UserService(
         return userRepo.findByIdOrNull(userId)?.toDto()
     }
     fun createUser(userDto: UserDto): UserDto {
-        val user = userRepo.save(userDto.toEntity()).toDto()
-        return user
+        try {
+            val user = userRepo.save(userDto.toEntity()).toDto()
+            return user
+        } catch (_: DataIntegrityViolationException) {
+            throw UserAlreadyExists()
+        }
     }
 }
