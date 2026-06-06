389a83f3f5f4fd137cd41fb9ad3f8c1b9cf32341
1-harshr <rharsh.iitk@gmail.com>
2026-06-07 00:59:42 +0530

    lint changes

--- src/main/kotlin/org/harsh/rbac/repo/NotesRepository.kt
+++ src/main/kotlin/org/harsh/rbac/repo/NotesRepository.kt
@@ -2,8 +2,6 @@ package org.harsh.rbac.repo
 
 import org.harsh.rbac.entity.NotesEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 
